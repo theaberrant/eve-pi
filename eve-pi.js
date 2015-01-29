3f6e620b170a7df8c88e894841f4d854579173cb
Tris <tris@theaberrant.com>
2015-01-28 22:40:01 -0600

    Redesign to output blueprint info rather than having multiple columns
    Removed Resources / P1 columns
    Renamed p1-list to output-list, widened column
    Using blueprints instead of having separate json for each PI production level
    Append div to output-list based on blueprint info
    Cleaned up CSS based on simpler design

--- eve-pi.js
+++ eve-pi.js
@@ -1,8 +1,5 @@
 var planetData = [];
-var resourceData = [];
-var p2Data = [];
-var selectedP1 = [];
-
+var blueprints = [];
 
 $.getJSON("data/planets.json", function (data) {
     $.each(data.planets, function (index, item) {
@@ -18,43 +15,11 @@ $.getJSON("data/planets.json", function (data) {
     })
 });
 
-$.getJSON("data/resources.json", function (data) {
-    console.log("Loading Resources...");
-    $.each(data.resources, function (index, item) {
-        $("#resource-list").append(
-            $('<div></div>')
-                .text(item.resource)
-                .attr("class", "resource_off")
-        );
-        resourceData.push(item)
-    })
-});
-
-$.getJSON("data/p1_basic.json", function (data) {
-    console.log("Loading P1 Basics...");
-    $.each(data.p1, function (index, item) {
-        $("#p1-list").append(
-            $('<div></div>')
-                .text(item)
-                .attr("class", "p1_off")
-        );
-    })
-});
-
-$.getJSON("data/p2_refined.json", function (data) {
-    console.log("Loading P2 Refined...");
+$.getJSON("data/blueprints.json", function (data) {
+    console.log("Loading Planetary blueprints...");
     $.each(data, function (index, item) {
-        $("#p2-list").append(
-            $('<div></div>')
-                .text(item.output)
-                .attr("class", "p2_off")
-        );
-        p2Data.push(item)
+        blueprints.push(item)
     })
-});
-
-$(document).ready(function () {
-//    console.log("Raw Data: " + data)
 });
 
 function processPlanet(clicked) {
@@ -69,74 +34,41 @@ function processPlanet(clicked) {
         if (item.type == clicked.innerHTML) {
             console.log("Planet Type: " + item.type);
             console.log("Planet Resources: " + item.resources);
-            processResources(item.resources);
+            findP1FromResources(item.resources);
         }
     })
 }
 
-function processResources(resources) {
-    // Reset resources
-    $("#resource-list").children("div").each(function (index, element) {
-        element.setAttribute('class', 'resource_off');
-    });
-
-    $.each(resources, function (index, resource) {
-        $("#resource-list").children("div").each(function (index, resourceElement) {
-            if (resourceElement.innerHTML == resource) {
-                resourceElement.setAttribute('class', 'resource_on');
-            }
-        });
-    });
-
-    var resourcesToProcess = [];
-    $.each(resourceData, function (index, resource) {
+function findP1FromResources(resources) {
+    var p1Blueprints = [];
+    $.each(blueprints, function (i1, blueprint) {
         $.each(resources, function (i2, calledResource) {
-            if (calledResource == resource.resource) {
-                resourcesToProcess.push(resource);
+            if (calledResource == blueprint.inputs[0]) {
+                console.log("Blueprint input " + calledResource + " found: " + blueprint);
+                p1Blueprints.push(blueprint);
             }
         })
     });
 
-    processP1(resourcesToProcess);
+    findP2FromBlueprints(p1Blueprints);
 }
 
-function processP1(resources) {
-    // Reset p1
-    $("#p1-list").children("div").each(function (index, element) {
-        element.setAttribute('class', 'p1_off');
-    });
+function findP2FromBlueprints(selectedP1s) {
+    // Reset Outputs
+    var p2ListNode = document.getElementById('output-list');
+    while (p2ListNode.firstChild) {
+        p2ListNode.removeChild(p2ListNode.firstChild);
+    }
 
-    // Update p1 background for matching resource p1 values
-    selectedP1 = [];
-    $.each(resources, function (index, resource) {
-        $("#p1-list").children("div").each(function (index, p1Element) {
-            if (p1Element.innerHTML == resource.p1) {
-                p1Element.setAttribute('class', 'p1_on');
-                selectedP1.push(resource.p1)
-            }
-        });
-    });
-
-    // Define p2 to be updated
-    processP2(selectedP1);
-    // Process P2
-}
-
-function processP2(selectedP1s) {
-    // Reset P2
-    $("#p2-list").children("div").each(function (index, element) {
-        element.setAttribute('class', 'p2_off');
-    });
-
-    var p2ToHighlight = [];
     var firstP1 = false;
-    $.each(p2Data, function (i2, p2) {
+    $.each(blueprints, function (i2, blueprint) {
         firstP1 = false;
-        $.each(p2.inputs, function (i3, p2Input) {
+        $.each(blueprint.inputs, function (i3, input) {
             $.each(selectedP1s, function (i1, p1) {
-                if (p2Input == p1) {
+                if (input == p1.output) {
                     if (firstP1) {
-                        p2ToHighlight.push(p2)
+                        console.log("Found blueprint: " + blueprint.output);
+                        displayBlueprint(blueprint);
                     } else {
                         firstP1 = true;
                     }
@@ -144,12 +76,29 @@ function processP2(selectedP1s) {
             })
         })
     });
+}
 
-    $.each(p2ToHighlight, function (index, p2) {
-        $("#p2-list").children("div").each(function (index, p2Element) {
-            if (p2Element.innerHTML == p2.output) {
-                p2Element.setAttribute('class', 'p1_on');
-            }
-        });
+function displayBlueprint(blueprint) {
+    var blueprintDiv = $('<div></div>')
+        .text(blueprint.output)
+        .attr("class", "output_on");
+    $.each(blueprint.inputs, function (i1, input) {
+        blueprintDiv.append(
+            $('<div></div>')
+                .text(input + " -> " + findBlueprintFromOutput(input).inputs)
+                .attr("class", "blueprint_inputs")
+            , null);
     });
+
+    $("#output-list").append(blueprintDiv);
+}
+
+function findBlueprintFromOutput(output) {
+    var outputBlueprint = null;
+    $.each(blueprints, function (i1, blueprint) {
+        if (blueprint.output == output) {
+            outputBlueprint = blueprint;
+        }
+    });
+    return outputBlueprint;
 }
