9070fb08d986844aba4edf0dca9aaa34b6659a65
Tris <tris@theaberrant.com>
2015-02-04 18:24:14 -0600

    Added images for blueprint outputs
    Supporting images pulled from Eve Online image server
    Some CSS organizing to make images not look as terrible (need to work on this)

--- eve-pi.js
+++ eve-pi.js
@@ -4,27 +4,33 @@ var blueprints = [];
 // Planet Data is hardcoded, based on http://wiki.eveuniversity.org/Planet
 $.getJSON("data/planets.json", function (data) {
     $.each(data.planets, function (index, item) {
-        $("#planet-list").append(
-            $('<div></div>')
-                .text(item.type)
-                .attr("class", "planet_deselected")
-                .click(function () {
-                    processPlanet(this)
-                })
-        );
+        var planetDiv = $('<div></div>')
+            .text(item.type)
+            .attr("class", "planet_deselected")
+            .click(function () {
+                processPlanet(this)
+            });
+
+
+        $("#planet-list").append(planetDiv);
         planetData.push(item)
     })
 });
 
 // Based on database static database query:
 /*
- SELECT CONCAT('{', GROUP_CONCAT(CONCAT('"', marketGroupName, '":', inputs) SEPARATOR ','), '}') as marketInputs FROM (
  SELECT
- marketGroupName, CONCAT('[', GROUP_CONCAT(inputs SEPARATOR ','), ']') as inputs
+ CONCAT('{',
+ '[',
+ GROUP_CONCAT(inputs
+ SEPARATOR ','),
+ ']',
+ '}') AS inputs
  FROM
  (SELECT
- mg_output.marketGroupID, mg_output.marketGroupName,
- CONCAT('{"output":', CONCAT('"', it_output.typeName, '",'), '"output_type":', CONCAT('"', mg_output.marketGroupName, '",'), '"inputs": [', GROUP_CONCAT(CONCAT('"', it_input.typeName, '"')
+ mg_output.marketGroupID,
+ mg_output.marketGroupName,
+ CONCAT('{"output":', CONCAT('"', it_output.typeName, '",'), '"output_type":', CONCAT('"', mg_output.marketGroupName, '",'), '"output_type_id":', CONCAT('"', it_output.typeID, '",'), '"inputs": [', GROUP_CONCAT(CONCAT('"', it_input.typeName, '"')
  SEPARATOR ','), ']}') inputs
  FROM
  planetSchematicsTypeMap ps_input
@@ -36,9 +42,7 @@ $.getJSON("data/planets.json", function (data) {
  ps_input.isInput = 1
  AND ps_output.isInput = 0
  GROUP BY mg_output.marketGroupId , it_output.typeName
- ORDER BY mg_output.marketGroupID DESC) AS input
- GROUP BY marketGroupID) as tbl_input
- GROUP BY marketGroupName;
+ ORDER BY mg_output.marketGroupID DESC) AS input;
  */
 $.getJSON("data/blueprints.json", function (data) {
     console.log("Loading Planetary blueprints...");
@@ -68,7 +72,7 @@ function findP1FromResources(resources) {
     var p1Blueprints = [];
     $.each(blueprints, function (i1, blueprint) {
         $.each(resources, function (i2, calledResource) {
-            if (calledResource == blueprint.inputs[0]) {
+            if (blueprint.inputs != null && calledResource == blueprint.inputs[0]) {
                 console.log("Blueprint input " + calledResource + " found: " + blueprint);
                 p1Blueprints.push(blueprint);
             }
@@ -88,34 +92,66 @@ function findP2FromBlueprints(selectedP1s) {
     var firstP1 = false;
     $.each(blueprints, function (i2, blueprint) {
         firstP1 = false;
-        $.each(blueprint.inputs, function (i3, input) {
-            $.each(selectedP1s, function (i1, p1) {
-                if (input == p1.output) {
-                    if (firstP1) {
-                        console.log("Found blueprint: " + blueprint.output);
-                        displayBlueprint(blueprint);
-                    } else {
-                        firstP1 = true;
+        if (blueprint.inputs != null) {
+            $.each(blueprint.inputs, function (i3, input) {
+                $.each(selectedP1s, function (i1, p1) {
+                    if (input == p1.output) {
+                        if (firstP1) {
+                            console.log("Found blueprint: " + blueprint.output);
+                            displayOutput(blueprint);
+                        } else {
+                            firstP1 = true;
+                        }
                     }
-                }
+                })
             })
-        })
+        }
     });
 }
 
-function displayBlueprint(blueprint) {
-    var blueprintDiv = $('<div></div>')
-        .text(blueprint.output)
-        .attr("class", "output");
-    $.each(blueprint.inputs, function (i1, input) {
-        blueprintDiv.append(
-            $('<div></div>')
-                .text(input + " -> " + findBlueprintFromOutput(input).inputs)
-                .attr("class", "blueprint_inputs")
-            , null);
-    });
+function displayOutput(blueprint) {
+    var blueprintDiv = getDivFromBlueprint(blueprint, 1);
 
     $("#output-list").append(blueprintDiv);
+}
+
+function getDivFromBlueprint(blueprint, level) {
+    var blueprintDiv = getOutputDiv(blueprint);
+
+    // Get div for each input and append
+    if (blueprint.inputs != null) {
+        var inputWrapperDiv = $('<div></div>')
+            .attr("class", "input_wrapper");
+
+        $.each(blueprint.inputs, function (i1, input) {
+            var inputBlueprint = findBlueprintFromOutput(input);
+            if (inputBlueprint != null) {
+                var inputDiv = getDivFromBlueprint(inputBlueprint, level + 1);
+                inputDiv.attr("class", "input");
+                inputWrapperDiv.append(inputDiv);
+            }
+        });
+        blueprintDiv.append(inputWrapperDiv);
+    }
+    return blueprintDiv
+}
+
+function getOutputDiv(blueprint) {
+    var returnDiv = $('<div></div>')
+        .attr("class", "output_wrapper");
+    var outputDiv = $('<div></div>')
+        .attr("class", "output");
+
+    var image = document.createElement('img');
+    image.setAttribute('src', getImageUrl(blueprint.output_type_id));
+    outputDiv.append(image);
+    var textDiv = $('<div></div>')
+        .text(blueprint.output)
+        .attr("class", "output_text");
+    outputDiv.append(textDiv);
+
+    returnDiv.append(outputDiv);
+    return returnDiv
 }
 
 function findBlueprintFromOutput(output) {
@@ -126,4 +162,8 @@ function findBlueprintFromOutput(output) {
         }
     });
     return outputBlueprint;
+}
+
+function getImageUrl(typeID) {
+    return "https://image.eveonline.com/Type/" + typeID + "_64.png"
 }
